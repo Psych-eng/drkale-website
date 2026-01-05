31674d0190f51aa8fb1173c80392cd5aedc469ce
Dr. Milind Kale <drkale@drkale.net>
2026-01-04 22:08:01 -0500

    Organize: Reorder districts alphabetically for better management

--- client/src/config/schoolPortal.ts
+++ client/src/config/schoolPortal.ts
@@ -19,26 +19,31 @@
  * - XXXX = 4 random characters (letters/numbers, excluding similar-looking chars)
  * - This format prevents accidental access to other districts even if mistyped
  * 
+ * ORGANIZATION:
+ * - Districts are listed in ALPHABETICAL ORDER for easy management
+ * - When adding new districts, insert them in alphabetical position
+ * - This makes it easy to find districts and prevents duplicates
+ * 
  * HOW TO ADD A NEW DISTRICT:
  * 1. Create a new folder in SharePoint for the district
  * 2. Generate "Anyone with the link" sharing link (Can edit, expiration 1/3/2027)
  * 3. Generate a random code: DISTRICTNAME-XXXX-2026 (use random generator)
- * 4. Add the mapping below: "DISTRICTNAME-XXXX-2026": "https://sharepoint-link"
+ * 4. Add the mapping below IN ALPHABETICAL ORDER by district name
  * 5. Update your tracking document with the access code
  * 6. Provide the access code to the district
  */
 
 export const schoolPortalConfig: Record<string, string> = {
-  // Active Districts (Secure Random Codes)
-  "HAMPTON-F5PM-2026": "https://drkale.sharepoint.com/:f:/s/MilindKaleMDLLC/IgDLZLNX4LlVS42l5xjGSz28AbWOWEi8KKnidmhWKCaF-bQ?e=cJ16ba", // Hampton
+  // Active Districts (Alphabetical Order)
   "AVON-QSXK-2026": "https://drkale.sharepoint.com/:f:/s/MilindKaleMDLLC/IgDiIh63SOeMRY6wHGmhunOgAcBtXVDInUbNglAvbM3nMS4?e=pgTB6X", // Avon
   "BLOOMFIELD-9UE3-2026": "https://drkale.sharepoint.com/:f:/s/MilindKaleMDLLC/IgAClVoPfdvxT4I5Gc57UT9FAWVvUHoDHPbkQCk3-VymYzo?e=iU9y0F", // Bloomfield
   "CANTON-AN6L-2026": "https://drkale.sharepoint.com/:f:/s/MilindKaleMDLLC/IgDcUS0Fa6WNSY-peVa9c6b5AeBjjjZI2uWOw4t-XkKWsfs?e=JwFdXX", // Canton
   "CREC-K7JH-2026": "https://drkale.sharepoint.com/:f:/s/MilindKaleMDLLC/IgBXPz70CWDqTLLqLHbMWTjlAcOqeVjWT1EHX3mBgSD3xu8?e=KLaRvm", // CREC
   "FARMINGTON-GHFH-2026": "https://drkale.sharepoint.com/:f:/s/MilindKaleMDLLC/IgClAd7GPS4JSoVk7OB4UzWRAZbSnuegOUKeVwcWZXovWt4?e=ybbkJU", // Farmington
-  "SOUTHWINDSOR-ZUZN-2026": "https://drkale.sharepoint.com/:f:/s/MilindKaleMDLLC/IgDnwZ3gCerGS7s-373xiDsWAVwE5s4MKhl4QRpoVE3TGmo?e=tkN70r", // South Windsor
-  "NEWINGTON-J5JU-2026": "https://drkale.sharepoint.com/:f:/s/MilindKaleMDLLC/IgC40_4FhcbQQbTfF2BA4ZmNAUtU9xA4OYdcklpfu_VQxDM?e=uMDjQY", // Newington
+  "HAMPTON-F5PM-2026": "https://drkale.sharepoint.com/:f:/s/MilindKaleMDLLC/IgDLZLNX4LlVS42l5xjGSz28AbWOWEi8KKnidmhWKCaF-bQ?e=cJ16ba", // Hampton
   "MIDDLETOWN-9EPJ-2026": "https://drkale.sharepoint.com/:f:/s/MilindKaleMDLLC/IgAvO2S08MD5QKyZiaS3AwbnAQNoGocwgZFArKXdJjd_vx8?e=CW4vd6", // Middletown
+  "NEWINGTON-J5JU-2026": "https://drkale.sharepoint.com/:f:/s/MilindKaleMDLLC/IgC40_4FhcbQQbTfF2BA4ZmNAUtU9xA4OYdcklpfu_VQxDM?e=uMDjQY", // Newington
+  "SOUTHWINDSOR-ZUZN-2026": "https://drkale.sharepoint.com/:f:/s/MilindKaleMDLLC/IgDnwZ3gCerGS7s-373xiDsWAVwE5s4MKhl4QRpoVE3TGmo?e=tkN70r", // South Windsor
   
   // Test/Demo Districts (for testing purposes)
   "TEST-SCHOOL-001": "https://drkale.sharepoint.com/sites/MilindKaleMDLLC/Shared%20Documents/School-Districts/Test-School-001",
@@ -46,18 +51,18 @@ export const schoolPortalConfig: Record<string, string> = {
 };
 
 /**
- * DISTRICT TRACKING REFERENCE:
+ * DISTRICT TRACKING REFERENCE (ALPHABETICAL ORDER):
  * (Keep this updated as you add new districts)
  * 
- * Hampton       - HAMPTON-F5PM-2026
  * Avon          - AVON-QSXK-2026
  * Bloomfield    - BLOOMFIELD-9UE3-2026
  * Canton        - CANTON-AN6L-2026
  * CREC          - CREC-K7JH-2026
  * Farmington    - FARMINGTON-GHFH-2026
- * South Windsor - SOUTHWINDSOR-ZUZN-2026
- * Newington     - NEWINGTON-J5JU-2026
+ * Hampton       - HAMPTON-F5PM-2026
  * Middletown    - MIDDLETOWN-9EPJ-2026
+ * Newington     - NEWINGTON-J5JU-2026
+ * South Windsor - SOUTHWINDSOR-ZUZN-2026
  * 
  * TOTAL ACTIVE DISTRICTS: 9
  * 
@@ -65,4 +70,6 @@ export const schoolPortalConfig: Record<string, string> = {
  * Use format: DISTRICTNAME-XXXX-2026
  * Where XXXX is 4 random characters from: ABCDEFGHJKLMNPQRSTUVWXYZ23456789
  * (Excludes: I, O, 0, 1 to avoid confusion)
+ * 
+ * Insert new districts in alphabetical order above.
  */
